2cf9ca9f0480210a979b3b7a54631fdbeb810ce4
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-11-17 17:18:15 +0700

    chore: change eslint and tailwind configs

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
