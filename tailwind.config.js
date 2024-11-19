a9cc4413181730bde8558aedd2c340047ed17315
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-11-20 00:28:39 +0700

    chore: config tailwind to compile code inside js files as well

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 export default {
-  content: ['./src/*.{html,js,css}', './src/views/**/*.ejs'],
+  content: ['./src/*.{html,js,css}', './src/views/**/*.ejs', './public/js/**/*.js'],
   theme: {
     extend: {
       transitionTimingFunction: {
@@ -7,7 +7,25 @@ export default {
       }
     }
   },
-  safelist: ['hidden', 'scale-y-0', 'scale-y-100'],
+  safelist: [
+    'hidden',
+    'scale-y-0',
+    'scale-y-100',
+    'max-h-48',
+    'rounded-lg',
+    'mx-auto',
+    'z-10',
+    'w-8',
+    ' h-8',
+    'text-gray-200',
+    'animate-spin',
+    'dark:text-gray-600',
+    'fill-blue-600',
+    'px-4',
+    'py-2',
+    'hover:bg-gray-100',
+    'cursor-pointer'
+  ],
   plugins: [
     {
       tailwindcss: {},
