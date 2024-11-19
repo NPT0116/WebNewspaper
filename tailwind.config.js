3671473df458d9d73c548224d5f6a901d457308a
mJota666 <micode2744@gmail.com>
2024-11-19 13:47:00 +0700

    landingPage 60%

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/*.{html,js,css}', './src/views/**/*.ejs'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Be VietNam', 'sans-serif'],
+        serif: ['EB Garamond', 'serif'],
+        eb: ['EB Garamond']
+      },
+      fontSize: {
+        '2xs': ['0.5rem', { lineHeight: '0.75rem' }]
+      }
+    }
   },
   plugins: [
     {
