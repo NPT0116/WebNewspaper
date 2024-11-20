0e3a49f255de19c1d2c9500c9656aa1495319b6a
mJota666 <micode2744@gmail.com>
2024-11-20 19:45:34 +0700

    pull git to do slug

--- src/controllers/landingpageController.ts
+++ src/controllers/landingpageController.ts
@@ -5,11 +5,11 @@ import { AppError } from '~/utils/appError.js';
 export const getLandingPage = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const data = await getLandingPageData();
-    // res.json(data);
-    res.render('layouts/LandingPageLayout/LandingPageLayout', {
-      body: '../../pages/LandingPage/LandingPage',
-      data
-    });
+    res.json(data);
+    // res.render('layouts/LandingPageLayout/LandingPageLayout', {
+    //   body: '../../pages/LandingPage/LandingPage',
+    //   data
+    // });
   } catch (error) {
     console.error(error);
     next(new AppError('Error fetching landing page data.', 500));
