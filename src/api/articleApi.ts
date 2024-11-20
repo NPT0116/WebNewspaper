da8fbc6658484b292992bd6c5d46962bff131186
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2024-11-20 23:50:38 +0700

    feat: api to get article of a reporter

--- src/api/articleApi.ts
+++ src/api/articleApi.ts
@@ -2,7 +2,8 @@ import express, { Response, Request, NextFunction } from 'express';
 import { PATH } from '~/config/path.js';
 export { queryArticleRouter, authorQueryArticleRouter };
 
-import { articleQuery, authorArticleQuery } from '~/controllers/articleController.js';
+import { articleQuery, authorArticleQuery, reporterGetArticleByIdQuery } from '~/controllers/articleController.js';
+import { verifyRole } from '~/middlewares/verifyRole.js';
 
 const queryArticleRouter = express.Router();
 
@@ -13,5 +14,6 @@ queryArticleRouter.get(PATH.HOME, articleQuery);
 authorQueryArticleRouter.get(PATH.HOME, authorArticleQuery);
 
 const articleApiRouter = express.Router();
+articleApiRouter.use(PATH.API.REPORTER.ARTICLE, verifyRole(['reporter']), reporterGetArticleByIdQuery);
 
 export default articleApiRouter;
