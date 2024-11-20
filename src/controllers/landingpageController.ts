0a9c066a155d1987fdec3409296f37b98f2eb570
mJota666 <micode2744@gmail.com>
2024-11-20 21:17:36 +0700

    additional return sectionTree in LandingPageController

--- src/controllers/landingpageController.ts
+++ src/controllers/landingpageController.ts
@@ -1,14 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import { Section } from '~/models/Section/sectionSchema.js';
 import { getLandingPageData } from '~/repo/Article/landingpage.js';
+import { getSectionTree } from '~/repo/Section/index.js';
 import { AppError } from '~/utils/appError.js';
 
 export const getLandingPage = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const data = await getLandingPageData();
-    // res.json(data);
+    const sectionTree = await getSectionTree();
+
     res.render('layouts/LandingPageLayout/LandingPageLayout', {
       body: '../../pages/LandingPage/LandingPage',
-      data
+      data: data,
+      section: sectionTree
     });
   } catch (error) {
     console.error(error);
