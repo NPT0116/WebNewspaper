ec0f1fc62e80dc8f4ecc7c0e6e0eb10df492af1b
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2024-11-24 00:18:48 +0700

    feat: add 50 articles and 54 profiles

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,22 +1,22 @@
-import { Request, Response, NextFunction } from 'express';
-import { ICommentResponse } from '~/interfaces/Comment/ commentInterface.js';
-import { getCommentsByArticleSlug } from '~/repo/comment/commentRepo.js';
-import { AppError } from '~/utils/appError.js';
+// import { Request, Response, NextFunction } from 'express';
+// import { ICommentResponse } from '~/interfaces/Comment/ commentInterface.js';
+// import { getCommentsByArticleSlug } from '~/repo/comment/commentRepo.js';
+// import { AppError } from '~/utils/appError.js';
 
-export const getCommentsByArticle = async (req: Request<{ sectionSlug: string; articleSlug: string }>, res: Response<ICommentResponse[]>, next: NextFunction) => {
-  try {
-    const { articleSlug } = req.params;
+// export const getCommentsByArticle = async (req: Request<{ sectionSlug: string; articleSlug: string }>, res: Response<ICommentResponse[]>, next: NextFunction) => {
+//   try {
+//     const { articleSlug } = req.params;
 
-    // Get comments from the service/repository
-    const comments: ICommentResponse[] = await getCommentsByArticleSlug(articleSlug);
+//     // Get comments from the service/repository
+//     const comments: ICommentResponse[] = await getCommentsByArticleSlug(articleSlug);
 
-    // Send the response with status 200
-    res.status(200).json(comments);
-  } catch (err) {
-    const statusCode = err instanceof AppError ? err.statusCode : 500;
-    const message = err instanceof AppError ? err.message : 'Error fetching comments';
+//     // Send the response with status 200
+//     res.status(200).json(comments);
+//   } catch (err) {
+//     const statusCode = err instanceof AppError ? err.statusCode : 500;
+//     const message = err instanceof AppError ? err.message : 'Error fetching comments';
 
-    // If the error has a status code, use it, else default to 500
-    next(new AppError(message, statusCode));
-  }
-};
+//     // If the error has a status code, use it, else default to 500
+//     next(new AppError(message, statusCode));
+//   }
+// };
