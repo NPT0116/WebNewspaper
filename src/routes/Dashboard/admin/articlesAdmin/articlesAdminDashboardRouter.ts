ed56453bfa0e4813978dda74f9087c3b6a0e307c
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2024-12-12 18:47:20 +0700

    feat: tag filter in search page

--- src/routes/Dashboard/admin/articlesAdmin/articlesAdminDashboardRouter.ts
+++ src/routes/Dashboard/admin/articlesAdmin/articlesAdminDashboardRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { PATH } from '~/config/path.js';
-import { adminDeleteArticle, renderAdminArticlePage } from '~/controllers/Dashboard/Admin/adminArticles/adminArticlesDashboardController.js';
+import { adminApprovalArticle, adminChangeSubscriptionArticle, adminDeleteArticle, renderAdminArticlePage } from '~/controllers/Dashboard/Admin/adminArticles/adminArticlesDashboardController.js';
 
 const articleAdminDashboardRouter = Router();
 
 articleAdminDashboardRouter.get('/', renderAdminArticlePage);
 articleAdminDashboardRouter.post(PATH.DASHBOARD.ADMIN.ARTICLES.DELETE, adminDeleteArticle);
+articleAdminDashboardRouter.post(PATH.DASHBOARD.ADMIN.ARTICLES.APPROVE, adminApprovalArticle);
+articleAdminDashboardRouter.get(PATH.DASHBOARD.ADMIN.ARTICLES.SUBSCRIPTION, adminChangeSubscriptionArticle);
+
+export default articleAdminDashboardRouter;
