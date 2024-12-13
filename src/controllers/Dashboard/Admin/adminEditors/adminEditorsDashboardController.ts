292aecd3e65133bd6d25d8d1de2f7b153d5f5bf7
pinkWar123 <131501454+pinkWar123@users.noreply.github.com>
2024-12-13 21:43:44 +0700

    feat: add deleteActive for editors

--- src/controllers/Dashboard/Admin/adminEditors/adminEditorsDashboardController.ts
+++ src/controllers/Dashboard/Admin/adminEditors/adminEditorsDashboardController.ts
@@ -5,7 +5,6 @@ import { IAccount } from '~/interfaces/Account/accountInterface.js';
 import { EditorProfile } from '~/models/Profile/editorProfile.js';
 import bcrypt from 'bcrypt';
 import { Article } from '~/models/Article/articleSchema.js';
-import { getSectionsList } from '~/repo/Section/index.js';
 import { Section } from '~/models/Section/sectionSchema.js';
 interface IEditorAdminDashboard {
   _id: mongoose.Types.ObjectId;
@@ -17,6 +16,7 @@ interface IEditorAdminDashboard {
     _id: mongoose.Types.ObjectId;
     name: string;
   };
+  deleteActivate: boolean;
 }
 
 export const renderAdminEditorPage = async (req: Request, res: Response) => {
@@ -32,7 +32,8 @@ export const renderAdminEditorPage = async (req: Request, res: Response) => {
       sectionId: {
         _id: editor.sectionId._id,
         name: editor.sectionId.name
-      }
+      },
+      deleteActivate: editor.editArticles.length === 0
     }));
 
     const sectionList = await Section.find({});
