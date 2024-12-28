eb6a383ba056ee3398c95daa28cbddbc0ddb38d5
mJota666 <micode2744@gmail.com>
2024-12-29 01:29:35 +0700

    adjust preview banner layout

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,10 @@ app.use(express.static('public'));
 
 app.use(flash()); // Thêm middleware flash
 
-app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(flash()); // Initialize connect-flash
 app.use(passport.initialize());
 
@@ -57,6 +58,7 @@ app.use(
     }
   })
 );
+
 
 app.use(passport.session());
 const io = configureSocketIO(server);
