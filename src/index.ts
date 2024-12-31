bbf74fa7507e7e5d34bb25827ada205ffe8029d4
NPT0116 <thanh1612004@gmail.com>
2024-12-31 13:57:13 +0700

    feat: pull main

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,8 @@ app.use(express.static('public'));
 
 app.use(flash()); // Thêm middleware flash
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(flash()); // Initialize connect-flash
 app.use(passport.initialize());
 
